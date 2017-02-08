552627bc973d3f43080eb7c3080ad2f01be07c57
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-07 19:26:58 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+        return { ...state, friends: [...state.friends, action.payload] }
+      break;
+    case 'REMOVE_FRIEND':
+        let updateFriends = state.friends.filter((friend) => friend.id !== action.payload)
+          return { ...state, friends: updateFriends }
+      break;
+    default:
+      return state;
+  }
 }
